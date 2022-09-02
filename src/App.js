ac28abe40f340faebab31685646caa09516f6e27
manohar72singh <53869936+manohar72singh@users.noreply.github.com>
2022-09-02 23:26:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CreateSurvey from './Components/CreateSurvey';
+import Header from './Components/Header';
+import LandingPage from './Components/LandingPage'
+import TakeSurvey from './Components/TakeSurvey';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {survey:false, question:[], createOrTake: true};
+
+  getQuestions = (questions) =>{
+      console.log("before=>", questions)
+      this.setState({question:questions, survey:false})
+  }
+
+  changeSurvey = (sur=true, val) => {
+      console.log("val=>",val)
+      this.setState({survey:sur, createOrTake:val})
+  }
+  render(){
+    console.log("app => ", this.state.survey)
+    return(
+      <>
+        <div className='container'>
+          <div className='row'>
+          <div className="col-lg-3 col-md-3"></div>
+          <div className="col-lg-6 col-md-6" style={{textAlign:"-webkit-center"}}>
+            <Header/>
+            {
+              !(this.state.survey) ?
+              <LandingPage changeSurvey = {this.changeSurvey} /> :
+              this.state.createOrTake ?
+              <CreateSurvey getQuestions={this.getQuestions} questions={this.state.question}/>:
+              <TakeSurvey changeSurvey={this.changeSurvey} questions={this.state.question}/>
+            }
+          </div>
+          <div className="col-lg-3 col-md-3"></div>
+          </div>
+        </div>
+      </>
+    )
+  }
 }
-
 export default App;
